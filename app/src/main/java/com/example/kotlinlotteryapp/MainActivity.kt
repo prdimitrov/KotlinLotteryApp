fdd0d625245d5f714f6d0340ce9818c62b25a0b8
DASKALA <prdimitrov97@gmail.com>
2025-01-12 15:55:00 +0200

    - Rotate kotlin image every one second - Change edittext hint color every one second - Add images - Finish the MainActivity layout.

--- app/src/main/java/com/example/kotlinlotteryapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlotteryapp/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.example.kotlinlotteryapp
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var kotlinImage: ImageView
+    private lateinit var usernameInput: EditText
+    private lateinit var getNumbersBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        kotlinImage = findViewById(R.id.kotlinImageView)
+        usernameInput = findViewById(R.id.usernameEditText)
+
+        val rotationAnimator = ObjectAnimator.ofFloat(kotlinImage, "rotation", 0f, 360f)
+        rotationAnimator.duration = 1000
+        rotationAnimator.repeatCount = ObjectAnimator.INFINITE
+        rotationAnimator.start()
+
+        val hintColorAnimator = ValueAnimator.ofArgb(
+            ContextCompat.getColor(this, android.R.color.white),
+            ContextCompat.getColor(this, android.R.color.black)
+        )
+        hintColorAnimator.duration = 1000
+        hintColorAnimator.repeatCount = ValueAnimator.INFINITE
+        hintColorAnimator.addUpdateListener { animator ->
+            val color = animator.animatedValue as Int
+            usernameInput.setHintTextColor(color)
         }
+        hintColorAnimator.start()
+
     }
 }
